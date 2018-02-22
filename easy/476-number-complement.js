4ee9ab5768abd3f9c54c080d3c0274398a0e0756
mpcen <vccengineers12@gmail.com>
2018-02-21 23:21:02 -0800

    added 476

---
+++ easy/476-number-complement.js
@@ -0,0 +1,32 @@
+/*
+
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+Note:
+The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+You could assume no leading zero bit in the integer’s binary representation.
+Example 1:
+Input: 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+Example 2:
+Input: 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+
+*/
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function(num) {
+    let binaryNum = num.toString(2);
+    let complement = '';
+    
+    for(let i = 0; i < binaryNum.length; i++) {
+        complement += binaryNum[i] === '1' ? '0' : '1'
+    }
+    
+    return parseInt(complement, 2);
+};
