8a453bf6be172c15030050baf2c38bbedbccd6c4
Emmanuel Martinez <emmanuel.martinez3@boeing.com>
2018-03-26 21:46:12 -0700

    added 572

---
+++ easy/572-subtree-of-another-tree.js
@@ -0,0 +1,62 @@
+/*
+
+Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+
+Example 1:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4 
+  / \
+ 1   2
+Return true, because t has the same structure and node values with a subtree of s.
+Example 2:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+Return false.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function(s, t) {
+    if(!s) return false;
+    
+    if(isSame(s, t)) return true;
+    
+    return isSubtree(s.left, t) || isSubtree(s.right, t);
+};
+
+const isSame = (s, t) => {
+    if(!s && !t) return true;
+    if(!s || !t) return false;
+    if(s.val !== t.val) return false;
+    
+    return isSame(s.left, t.left) && isSame(s.right, t.right);
+}
