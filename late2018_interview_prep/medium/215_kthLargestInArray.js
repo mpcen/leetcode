41601a528e5c64024449fcb484230deef12b11b5
Manny Martinez <vccengineers12@gmail.com>
2018-12-29 02:38:11 -0800

    Added O(n) solution of 215

--- late2018_interview_prep/medium/215_kthLargestInArray.js
+++ late2018_interview_prep/medium/215_kthLargestInArray.js
@@ -17,7 +17,7 @@
  * @param {number} k
  * @return {number}
  */
-var findKthLargest = function(nums, k) {
+var findKthLargest_Onlogn = function(nums, k) {
     nums = sort(nums);
     return nums[k-1]
 };
@@ -31,3 +31,79 @@ function sort(nums) {
 }
 
 // Solution 2: O(n)
+var findKthLargest_On = function(nums, k) {
+    let minHeap = new MinHeap(k);
+    
+    for(let i = 0; i < nums.length; i++) {
+        minHeap.add(nums[i]);
+        if(minHeap.size > k) minHeap.poll();
+    }
+    
+    return minHeap.poll();
+};
+
+class MinHeap {
+    constructor(maxSize) {
+        this.items = [];
+        this.size = 0;
+        this.maxSize = maxSize;
+    }
+    
+    getLeftChildIndex(index) { return 2*index+1; }
+    getRightChildIndex(index) { return 2*index+2; }
+    getParentIndex(index) { return Math.ceil((index - 2)/2); }
+    hasLeftChild(index) { return this.getLeftChildIndex(index) < this.size; }
+    hasRightChild(index) { return this.getRightChildIndex(index) < this.size; }
+    hasParent(index) { return this.getParentIndex(index) >= 0; }
+    leftChild(index) { return this.items[this.getLeftChildIndex(index)]; }
+    rightChild(index) { return this.items[this.getRightChildIndex(index)]; }
+    parent(index) { return this.items[this.getParentIndex(index)]; }
+    
+    add(item) {
+        this.items.push(item);
+        this.size++;
+        this.heapifyUp();
+    }
+    
+    poll() {
+        let item = this.items[0];
+        this.items[0] = this.items.pop();
+        this.size--;
+        this.heapifyDown();
+        return item;
+    }
+    
+    heapifyUp() {
+        let index = this.size - 1;
+        
+        while(this.hasParent(index) && this.items[index] < this.parent(index)) {
+            this.swap(index, this.getParentIndex(index));
+            index = this.getParentIndex(index);
+        }
+    }
+    
+    heapifyDown() {
+        let index = 0;
+        
+        while(this.hasLeftChild(index)) {
+            let smallerChildIndex = this.getLeftChildIndex(index);
+            
+            if(this.hasRightChild(index) && this.rightChild(index) < this.leftChild(index)) {
+                smallerChildIndex = this.getRightChildIndex(index);
+            }
+            
+            if(this.items[index] < this.items[smallerChildIndex]) {
+                break;
+            } else {
+                this.swap(smallerChildIndex, index);
+                index = smallerChildIndex;
+            }
+        }
+    }
+    
+    swap(a, b) {
+        let tmp = this.items[a];
+        this.items[a] = this.items[b];
+        this.items[b] = tmp;
+    }
+}
