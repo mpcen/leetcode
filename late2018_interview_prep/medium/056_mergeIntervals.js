77030904662fb921772f4c27aefda3e541704747
Manny Martinez <vccengineers12@gmail.com>
2019-01-26 20:49:54 -0800

    modified 056

--- late2018_interview_prep/medium/056_mergeIntervals.js
+++ late2018_interview_prep/medium/056_mergeIntervals.js
@@ -1,40 +1,60 @@
-const merge = intervals => {
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {Interval[]}
+ 
+ 
+ [[1, 4], [2, 3]] => [[1, 4]]
+ 
+ startTimes: [1, 2]
+                 i
+ 
+ endTimes:   [3, 4]
+                 i
+              
+ merged:      [[1, 4]]
+
+ 
+ */
+var merge = function(intervals) {
     if(!intervals.length || intervals.length === 1) return intervals;
     
-    const cache = { min: Number.MAX_SAFE_INTEGER, max: Number.MIN_SAFE_INTEGER };
-    const mergedIntervals = [];
+    let merged = [];
+    let startTimes = [], endTimes = [];
     
-    intervals = sortIntervals(intervals);
+    intervals = sort(intervals);
     
-    intervals.forEach(interval => {
-       if(interval.start < cache.min && interval.end > cache.max) {
-           cache.min = interval.start;
-           cache.max = interval.end;
-       } else if(interval.start > cache.max) {
-           mergedIntervals.push(new Interval(cache.min, cache.max));
-           cache.min = interval.start;
-           cache.max = interval.end;
-       } else if(interval.end > cache.max) cache.max = interval.end;
-    });
-
-    mergedIntervals.push(new Interval(cache.min, cache.max));
+    for(let i = 0; i < intervals.length; i++) {
+        startTimes.push(intervals[i].start);
+        endTimes.push(intervals[i].end);
+    }
     
-    return mergedIntervals;
+    endTimes = sort(endTimes);
+    startTimes = sort(startTimes);
+    
+    merged.push(new Interval(startTimes[0], endTimes[0]));
+    
+    for(let i = 1; i < startTimes.length; i++) {
+        if(startTimes[i] <= merged[merged.length - 1].end) {
+            merged[merged.length - 1].end = endTimes[i];
+        } else {
+            merged.push(new Interval(startTimes[i], endTimes[i]));
+        }
+    }
+    
+    return merged;
 };
 
-function sortIntervals(intervals) {
+function sort(intervals) {
     return intervals.sort((a, b) => {
-        if(a.start <= b.start) return -1;
-        if(a.start > b.start) return 1;
+        if(a.start < b.start || a < b) return -1;
+        if(a.start > b.start || a > b) return 1;
         return 0;
     });
 }
-
-class Interval {
-    constructor(start, end) {
-        this.start = start;
-        this.end = end;
-    }
-}
-
-console.log(merge([new Interval(1,1), new Interval(1,15), new Interval(8, 10), new Interval(15, 18)]));
